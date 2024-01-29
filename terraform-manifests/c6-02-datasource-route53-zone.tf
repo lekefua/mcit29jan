56ea7419f18502a49edf403b5921a5ef6a42e70d
Sami Islam <islam.samipkr@gmail.com>
2024-01-29 17:55:02 -0500

    Update c6-02-datasource-route53-zone.tf

--- terraform-manifests/c6-02-datasource-route53-zone.tf
+++ terraform-manifests/c6-02-datasource-route53-zone.tf
@@ -1,14 +1,7 @@
 # Get DNS information from AWS Route53
-/*data "aws_route53_zone" "mydomain" {
+data "aws_route53_zone" "mydomain" {
   name         = "devopsincloud.com"
-}*/
-resource "aws_route53_record" "mcittest" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.mcitdevops.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+
 # Output MyDomain Zone ID
 output "mydomain_zoneid" {
   description = "The Hosted Zone id of the desired Hosted Zone"
